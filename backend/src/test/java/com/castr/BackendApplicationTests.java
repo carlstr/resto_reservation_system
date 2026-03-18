bf2100d14412330c424f4cf8c899e58b7c163697
carl <carlstromberg3@gmail.com>
2026-03-18 18:31:30 +0200

    initial commit

---
+++ backend/src/test/java/com/castr/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.castr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
